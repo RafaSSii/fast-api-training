863e06b5dac8dd850157c95114355fb5c6da02b4
RafaSSii <rafarsk78@gmail.com>
2026-08-16 09:07:13 -0300

    feat: Added account creation and database dependencies

--- auth_routes.py
+++ auth_routes.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter
+from models import User
+from dependencies import sessionmaker, take_session
 
 #Criação do roteador de rotas de autenticação
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def login():
+async def home():
     """
     Essa é a rota padrão de autenticação do sistema
     """
     return {"Message": "Você acessou o login", "Autenticado": False}
+
+@auth_router.post("/create_account")
+async def create_account(email: str, password: str, name: str, session = take_session()):
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        return {"mensagem: Já existe um usuário com esse email"}
+    else:
+        new_user = User(name=name, email=email, password=password)
+        session.add(new_user)
+        session.commit()
+        return {"mensagem: Usuário cadastrado com sucesso"}
